b5f3889847dadb33a33e6893c426496b0ae95951
katlegoprince1998 <princekatlego357@gmail.com>
2024-03-15 10:11:32 +0200

    Project set up

---
+++ src/main/java/com/groupg/eparkingchallan/EParkingChallanApplication.java
@@ -0,0 +1,13 @@
+package com.groupg.eparkingchallan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EParkingChallanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EParkingChallanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupg/eparkingchallan/EParkingChallanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupg.eparkingchallan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EParkingChallanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
